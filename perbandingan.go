01eaaf32a47dcd78f44aa61dc33e3bd21cdb2e91
armanmaulana123 <armanmaulanasaputra15@gmail.com>
2025-02-03 07:14:05 +0700

    0.2.0 perbandingan

---
+++ perbandingan.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "Arman"
+	var name2 = "Arman"
+
+	var result bool = name1 == name2
+	var result2 bool = name1 != name2
+	fmt.Println(result)
+	fmt.Println(result2)
+}
